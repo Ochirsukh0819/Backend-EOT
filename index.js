480c7a2a7d8b7f800fa70c3190b3cbf35dd71e9f
Ochirsukh0819 <oojoo20020819@gmail.com>
2023-05-15 17:42:56 +0800

    a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { Pool } = require("pg");
 const cors = require("cors");
+const fetch = require("node-fetch");
 
 const app = express();
 const port = 3000;
