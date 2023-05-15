850b2b943ce5cda53e41f6ca6756a34b52097fec
Ochirsukh0819 <oojoo20020819@gmail.com>
2023-05-15 17:45:00 +0800

    delete node fetch

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { Pool } = require("pg");
 const cors = require("cors");
-const fetch = require("node-fetch");
 
 const app = express();
 const port = 3000;
